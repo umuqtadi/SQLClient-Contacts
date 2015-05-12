db3c5577fca8d40221a89b3b464392d38232b60e
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-05-12 17:06:56 -0600

    Initial Commit

--- SqlClient-Contacts/SqlClient-Contacts/Models/Contact.cs
+++ SqlClient-Contacts/SqlClient-Contacts/Models/Contact.cs
@@ -8,6 +8,18 @@ namespace SqlClient_Contacts.Models
     public class Contact
     {
         //TODO: Fill in the contact class
-      
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+
+        public Contact() { }
+        public Contact(int contactID, string name, string email, string title)
+        {
+            this.ContactID = contactID;
+            this.Name = name;
+            this.Email = email;
+            this.Title = title;
+        }
     }
 }
--- SqlClient-Contacts/SqlClient-Contacts/Models/ContactDataAccess.cs
+++ SqlClient-Contacts/SqlClient-Contacts/Models/ContactDataAccess.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
 using System.Data.SqlClient;
 
 namespace SqlClient_Contacts.Models
@@ -12,19 +11,69 @@ namespace SqlClient_Contacts.Models
         public static bool InsertContact(string name, string email, string title)
         {
             //TODO: INSERT contact in the database
-            return false;
+            using (SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                con.Open();
+                try
+                {
+                    SqlCommand command = new SqlCommand("INSERT INTO Contacts (Name, Email, Title) Values (@name, @email, @title)", con);
+                    command.Parameters.Add(new SqlParameter("name", name));
+                    command.Parameters.Add(new SqlParameter("email", email));
+                    command.Parameters.Add(new SqlParameter("title", title));
+
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch
+                {
+
+                    return false;
+                }
+            }
         }
 
         public static bool DeleteContact(int id)
         {
             //TODO: DELETE contact in the database by ID
-            return false;
+            using (SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                con.Open();
+                try
+                {
+                    SqlCommand command = new SqlCommand("DELETE FROM Contacts (ContactID) Values (@contactID)", con);
+                    command.Parameters.Add(new SqlParameter("contactID", id));
+                 
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch
+                {
+
+                    return false;
+                }
+            }
         }
 
         public static Contact GetContactById(int id)
         {
             //TODO: SELECT a contact from the database by Id
-            return new Contact();
+            using (SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                con.Open();
+                try
+                {
+                    SqlCommand command = new SqlCommand("SELECT @contactID FROM Contacts", con);
+                    command.Parameters.Add(new SqlParameter("contactID", id));
+
+                    command.ExecuteNonQuery();
+                    return new Contact();
+                }
+                catch
+                {
+
+                    return new Contact();
+                }
+            }
         }
 
         public static List<Contact> GetAllContacts() 
